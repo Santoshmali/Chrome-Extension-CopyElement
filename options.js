ff8943ac34e6582dcdbaae9c24fd490f6ba277a6
Santosh Mali <santoshmalisp@gmail.com>
2021-12-02 12:50:08 +0530

    Add files via upload

---
+++ options.js
@@ -0,0 +1,16 @@
+let elementIdToCopy = document.getElementById("copyelmentid");
+let buttonApply = document.getElementById("btnApply");
+
+
+function handleApplyButtonClick() {
+    alert(elementIdToCopy.value);
+    setElementId();
+}
+
+function setElementId() {
+    let copyelementid = elementIdToCopy.value;
+    chrome.storage.sync.set({ copyelementid });
+}
+
+buttonApply.addEventListener("click", handleApplyButtonClick);
+setElementId();
---
+++ popup.js
@@ -0,0 +1,28 @@
+let copyelement = document.getElementById("copyelmentid");
+let btnCopyElement = document.getElementById("btnCopyElement");
+
+chrome.storage.sync.get("copyelementid", ({ copyelementid }) => {
+    copyelement.innerText = copyelementid;
+});
+
+btnCopyElement.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: getElementHtml,
+    });
+});
+
+function getElementHtml() {
+    chrome.storage.sync.get("copyelementid", ({ copyelementid }) => {
+        let element = document.getElementById(copyelementid);
+        if (element) {
+            navigator.clipboard.writeText(element.innerHTML);
+            alert('Inner html of "' + copyelementid  +'" copied to clipboard!');
+        }
+        else {
+            alert('Element not found');
+        }
+    });
+}
